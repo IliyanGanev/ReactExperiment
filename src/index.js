b0b03a4b96aaf884ede05c726497f94b37e6d699
Iliyan Ganev <ganeviliyan@gmail.com>
2017-11-10 22:05:24 -0800

    Start with components and state

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import ReactDOM from 'react-dom'
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar'; 
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//const API_KEY = 'AIzaSyAx8_7aPRG3KY6Hc-X4SxL0VZ1UA2uRRsE';
+// Create a new conponent that produces some HTML.
+const App = () => {
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
+}
+// Take this component and put it on the page(DOM).
+ReactDOM.render(<App />, document.querySelector('.container') );
