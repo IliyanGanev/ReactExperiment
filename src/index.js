5149d9a3e517aa82117c3a8bff9762faa21665e5
Iliyan Ganev <ganeviliyan@gmail.com>
2017-11-13 17:28:42 -0800

    Get info from Youtube API

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
-
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search'; 
 import SearchBar from './components/search_bar'; 
+import VideoList from './components/video_list';
+const API_KEY = 'AIzaSyAx8_7aPRG3KY6Hc-X4SxL0VZ1UA2uRRsE';
 
-//const API_KEY = 'AIzaSyAx8_7aPRG3KY6Hc-X4SxL0VZ1UA2uRRsE';
-// Create a new conponent that produces some HTML.
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
-}
-// Take this component and put it on the page(DOM).
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { videos:[] };
+		 
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+	 		this.setState({ videos });
+	 		// videos: videos
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoList videos={this.state.videos} />
+			</div>
+		);
+	}
+} 
+
+
 ReactDOM.render(<App />, document.querySelector('.container') );
